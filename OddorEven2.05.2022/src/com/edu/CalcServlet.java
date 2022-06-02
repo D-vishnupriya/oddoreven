43db8bb6cfe2c3e43c1fb281d249fd35955eedd5
HP <HP@DESKTOP-JTVDBF5>
2022-06-02 20:21:21 +0530

    Odd or Even

---
+++ OddorEven2.05.2022/src/com/edu/CalcServlet.java
@@ -0,0 +1,50 @@
+package com.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+@WebServlet("/CalcServlet")
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		
+
+int number=Integer.parseInt(request.getParameter("number"));
+
+
+if(number%2==0)
+{
+out.println("It is an even number");
+}
+else
+{
+	out.println("It is an odd number");
+}
+	}
+}
+	
+
